5383d7302b096d90a840cb8668732edee71a9689
josh_07 <joshuadeveloper07@gmail.com>
2023-06-17 16:54:39 +0800

    commit message

---
+++ qr-code-component/src/pages/QrCode.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-layout class="background">
+        <div class="flex flex-center">
+            <q-card class="side my-card">
+                <div class="image-padding">
+                    <q-img class="image-side" src="../pages/images/image-qr-code.png"
+                    />
+                </div>
+                <div class="text-center font-text q-px-md q-pt-sm q-pb-lg  q-gutter-y-md">
+                    <div class="text-size">Improve your front-end skills by building projects</div>
+                    <div class="text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
+                </div>
+            </q-card>
+        </div>
+    </q-layout>
+</template>
+
+<style>
+@import url('../pages/styles/style.scss');
+</style>
